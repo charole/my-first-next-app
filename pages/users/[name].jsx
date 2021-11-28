f27da66b4a51e0b571bce93ad1a9514f8f45f40e
charole <rjs985939@gmail.com>
2021-11-28 22:57:26 +0900

    추가.
    paging 추가.
    repositories 컴포넌트 추가.

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -4,67 +4,12 @@ import axios from 'axios';
 import Profile from '../../components/Profile';
 import css from 'styled-jsx/css';
 import formatDistance from 'date-fns/formatDistance';
+import Repositories from '../../components/Repositories';
 
 const style = css`
   .user-contents-wrapper {
     display: flex;
     padding: 20px;
-  }
-  .repos-wrapper {
-    width: 100%;
-    height: calc(100vh - 40px);
-    overflow: scroll;
-    padding: 0px 16px;
-  }
-  .repos-header {
-    padding: 16px 0;
-    font-size: 14px;
-    font-weight: 600;
-    border-bottom: 1px solid #e1e4e8;
-  }
-  .repos-count {
-    display: inline-block;
-    padding: 2px 5px;
-    margin-left: 6px;
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 1;
-    color: #586069;
-    background-color: rgba(27, 31, 35, 0.08);
-    border-radius: 20px;
-  }
-
-  .repository-wrapper {
-    width: 100%;
-    border-bottom: 1px solid #e1e4e8;
-    padding: 24px 0;
-  }
-  .repository-description {
-    padding: 12px 0;
-  }
-  a {
-    text-decoration: none;
-  }
-  .repository-name {
-    margin: 0;
-    color: #0366d6;
-    font-size: 20px;
-    display: inline-block;
-    cursor: pointer;
-  }
-  .repository-name:hover {
-    text-decoration: underline;
-  }
-  .repository-description {
-    margin: 0;
-    font-size: 14px;
-  }
-  .repository-language {
-    margin: 0;
-    font-size: 14px;
-  }
-  .repository-updated-at {
-    margin-left: 20px;
   }
 `;
 
@@ -76,33 +21,7 @@ const Name = ({ user, repos }) => {
     <>
       <div className="user-contents-wrapper">
         <Profile user={user} />
-        <div className="repos-wrapper">
-          <div className="repos-header">
-            Repositories<span className="repos-count">{user.public_repos}</span>
-          </div>
-          {user &&
-            repos &&
-            repos.map((repo) => (
-              <div key={repo.id} className="repository-wrapper">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href={`https://github.com/${user.login}/${repo.name}`}
-                >
-                  <h2 className="repository-name">{repo.name}</h2>
-                </a>
-                <p className="repository-description">{repo.description}</p>
-                <p className="repository-language">
-                  {repo.language}
-                  <span className="repository-updated-at">
-                    {formatDistance(new Date(repo.updated_at), new Date(), {
-                      addSuffix: true,
-                    })}
-                  </span>
-                </p>
-              </div>
-            ))}
-        </div>
+        <Repositories user={user} repos={repos} />
       </div>
       <style jsx>{style}</style>
     </>
@@ -110,7 +29,7 @@ const Name = ({ user, repos }) => {
 };
 
 export const getServerSideProps = async ({ query }) => {
-  const { name } = query;
+  const { name, page } = query;
   try {
     let user;
     let repos;
@@ -123,7 +42,7 @@ export const getServerSideProps = async ({ query }) => {
       .catch((err) => err);
     const repoRes = await axios
       .get(
-        `https://api.github.com/users/${name}/repos?sort=updated&page=1&per_page=10`
+        `https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`
       )
       .then((res) => {
         repos = res.data;
